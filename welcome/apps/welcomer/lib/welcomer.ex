10461951f7c7dabdaccaa6041957f4bdae09b7bc
Victor Moreno <victor.moreno@cabify.com>
2024-02-01 18:53:42 +0100

    chore: remove code to create the learning path

--- welcome/apps/welcomer/lib/welcomer.ex
+++ welcome/apps/welcomer/lib/welcomer.ex
@@ -1,5 +1,5 @@
 defmodule Welcomer do
-  @clock Application.compile_env(:welcomer, :clock)
+  # @clock Application.compile_env(:welcomer, :clock)
   def hello do
     "Hello, world!"
   end
@@ -14,16 +14,9 @@ defmodule Welcomer do
 
   def helloAt(_who, {h, m, s}) do
     # Implement this function
-    if h < 12 do
-      "Good morning, #{_who}!, the time is #{h}:#{m}:#{s}"
-    else
-      "Good afternoon, #{_who}!"
-    end
   end
 
   def helloAt(_who) do
     # Implement this function
-    {h, m, s} = @clock.time()
-    helloAt(_who, {h, m, s})
   end
 end
