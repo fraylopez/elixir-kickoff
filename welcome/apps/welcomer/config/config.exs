5481554ec0bd341c02ea5ea1d8f7aaab38ec257d
Victor Moreno <victor.moreno@cabify.com>
2024-02-01 10:52:55 +0100

    refactor: remove internal configs

--- welcome/apps/welcomer/config/config.exs
+++
@@ -1,4 +0,0 @@
-import Config
-
-# config :welcomer, env: config_env()
-# config(:welcomer, clock: Welcomer.SystemClock)
--- welcome/apps/welcomer/config/test.exs
+++
@@ -1,2 +0,0 @@
-import Config
-# config(:welcomer, clock: TestClock, current_time: {19, 0, 0})
