144a449efe0b131855623643bfbd3e7b4e0fda08
Victor Moreno <victor.moreno@cabify.com>
2024-02-02 18:15:18 +0100

    chore: remove code to be implemented

--- welcome/apps/cli/lib/cli.ex
+++ welcome/apps/cli/lib/cli.ex
@@ -30,7 +30,6 @@ defmodule Cli do
     IO.puts(" 1. Greet the world")
     IO.puts(" 2. Greet someone")
     IO.puts(" 3. Greet someone with a message")
-    IO.puts(" 4. Greet someone at the current time")
 
     IO.gets("Please enter a number:")
   end
@@ -38,7 +37,6 @@ defmodule Cli do
   defp handle_input("1"), do: greet_world()
   defp handle_input("2"), do: greet_someone()
   defp handle_input("3"), do: greet_someone_with_message()
-  defp handle_input("4"), do: greet_someone_at_current_time()
   defp handle_input(_), do: IO.puts("Invalid input!")
 
   defp greet_world do
@@ -62,15 +60,6 @@ defmodule Cli do
       |> String.trim()
 
     Welcomer.hello(who, message)
-    |> IO.puts()
-  end
-
-  defp greet_someone_at_current_time do
-    who =
-      IO.gets("Who do you want to greet?")
-      |> String.trim()
-
-    Welcomer.helloAt(who)
     |> IO.puts()
   end
 end
